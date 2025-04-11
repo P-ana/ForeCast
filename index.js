44b932b78f3f3a042317f2e09dc09673d0fb678b
P-ana <anarakic.a@gmail.com>
2025-04-11 12:56:59 +0200

    feat: display time and city on home  page

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app=express();
 /*process.env is a built-in Node.js object that stores environment variables.
 After using dotenv.config(), it loads everything from your .env file into process.env.*/
 const port=process.env.PORT || 3000;
-const API_URL='https://api.openweathermap.org/data/2.5/weather';
+const API_URL='https://api.openweathermap.org/data/2.5/forecast';
 const API_KEY=process.env.API_KEY;
 
 
@@ -24,8 +24,38 @@ const config = {
   };
 
 
-app.get("/", (req, res) => {
-        res.render("index", { content: "Waiting for data..." });
+app.get("/", async (req, res) => {
+  try{
+    const response= await axios.get(`${API_URL}?lat=46.0500268&lon=14.5069289&units=metric&appid=${API_KEY}`,config);
+    const list= response.data.list;
+    let weather=list[0].weather[0].main;
+    let temp=list[0].main.temp;
+    temp= `${temp}°C`;
+    const city=response.data.city.name;
+    const icon= list[0].weather[0].icon;
+    const iconUrl=`https://openweathermap.org/img/wn/${icon}@2x.png`;
+   
+    const listData=response.data.list.slice(1,5);
+    const hourData = listData.map(item => ({
+      temp: item.main.temp,
+      hour: item.dt_txt,
+      icon: item.weather[0].icon
+    }));
+    
+     
+    res.render("index",{
+      weather,
+      temp,
+      city,
+      icon:iconUrl,
+      hourData
+    });
+  }
+
+  catch (error){
+    res.render("index", { content: JSON.stringify(error.response.data) });
+  }
+        
       });   
     
 

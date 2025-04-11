90187e3befb2c3c284682d0f0924627862c1e588
P-ana <anarakic.a@gmail.com>
2025-04-11 14:17:11 +0200

    feat: display time and city user choice

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ app.get("/", async (req, res) => {
       temp,
       city,
       icon:iconUrl,
-      hourData
+      hourData,
+      userSearch: false
     });
   }
 
@@ -59,7 +60,45 @@ app.get("/", async (req, res) => {
       });   
     
 
+app.get('/submit', async (req, res) => {
+ try{
+  const cityName=req.query.city?.trim();
+  const geoUrl=`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${API_KEY}`
+  const geoResponse=await axios.get(geoUrl);
+  if (geoResponse.data.length === 0) {
+    return res.render("index", { error: "City not found." })};
+  
+  const lat=geoResponse.data[0].lat;
+  const lon=geoResponse.data[0].lon;
+  
+  const response= await axios.get(`${API_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`,config);
+  
+  const list= response.data.list;
+  let weather=list[0].weather[0].main;
+  let temp=list[0].main.temp;
+  temp= `${temp}°C`;
+  const city=response.data.city.name;
+  const icon= list[0].weather[0].icon;
+  const iconUrl=`https://openweathermap.org/img/wn/${icon}@2x.png`;
+   
+  const listData=response.data.list.slice(1,5);
+  const hourData = listData.map(item => ({
+      temp: item.main.temp,
+      hour: item.dt_txt,
+      icon: item.weather[0].icon
+}))
+res.render("index", {
+  weather,
+  temp,
+  city,
+  icon: iconUrl,
+  hourData,
+  userSearch: true
 
+})}
+catch(error){
+  res.render("index", { content: JSON.stringify(error.response.data) });
+}});
 
 
 

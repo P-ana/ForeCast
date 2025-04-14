74313cddd95fe1d41cb2b2f128c138f828e8631b
P-ana <anarakic.a@gmail.com>
2025-04-14 10:39:59 +0200

    debug

--- index.js
+++ index.js
@@ -19,54 +19,36 @@ app.set('view engine', 'ejs');
 
 
 
-app.get('/submit', async (req, res) => {
-  console.log('Inside /submit route');  // Log when the route is hit
-  
-  try {
-    const cityName = req.query.city?.trim();
-    const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${API_KEY}`;
-    const geoResponse = await axios.get(geoUrl);
+app.get("/", async (req, res) => {
+  try{
+    const response= await axios.get(`${API_URL}?lat=46.0500268&lon=14.5069289&units=metric&appid=${API_KEY}`)
+    const list= response.data.list;
+    let weather=list[0].weather[0].main;
+    let temp=list[0].main.temp;
+    temp= `${temp}°C`;
+    const city=response.data.city.name;
+    const icon = list[0].weather[0].icon || '01d'; 
+    const iconUrl=`https://openweathermap.org/img/wn/${icon}@2x.png`;
     
-     
-    
-    if (geoResponse.data.length === 0) {
-      
-      return res.render("index", { error: "City not found.", userSearch: false });
-    }
-
-    const lat = geoResponse.data[0].lat;
-    const lon = geoResponse.data[0].lon;
-    
-    const response = await axios.get(`${API_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`);
-    
-    
-    const list = response.data.list;
-    let weather = list[0].weather[0].main;
-    let temp = list[0].main.temp;
-    temp = `${temp}°C`;
-    const city = response.data.city.name;
-    const icon = list[0].weather[0].icon;
-    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-
-    const listData = response.data.list.slice(1, 5);
+   
+    const listData=response.data.list.slice(1,5);
     const hourData = listData.map(item => ({
       temp: item.main.temp,
       hour: item.dt_txt,
       icon: item.weather[0].icon
     }));
-
     
-    res.render("index", {
+   
+    res.render("index",{
       weather,
       temp,
       city,
-      icon: iconUrl,
+      icon:iconUrl,
       hourData,
-      userSearch: true
-    });
-  } catch (error) {
-    console.log('Error:', error);  // Log any error during the request
+      userSearch: false
+    });}
 
+  catch (error){
     res.render("index", {
       weather: null,
       temp: null,
@@ -77,8 +59,9 @@ app.get('/submit', async (req, res) => {
       error: JSON.stringify(error.response?.data || "An error occurred")
     });
   }
-});
-
+        
+      });   
+    
 
 app.get('/submit', async (req, res) => {
  try{
